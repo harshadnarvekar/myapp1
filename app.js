271512fb609bdda8be0474897145629b620e247c
Harshad <harshad.narvekar@tabcorp.com.au>
2017-06-10 16:24:25 +1000

    my first push

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World! my first change')
 })
 
 app.post('/', function (req, res) {
-  res.send('Got a POST request')
+  res.send('Got a POST request! my first change')
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000! my first change')
 })
